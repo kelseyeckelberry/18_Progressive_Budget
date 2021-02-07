21c740eee44601e14fae7929f301a43bd37f50a6
kelseck <kelseyeckelberry@gmail.com>
2021-02-06 20:25:59 -0600

    db / OUN + OSS

---
+++ public/db.js
@@ -0,0 +1,15 @@
+let db;
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  db.createObjectStore('pending', { autoIncrement: true });
+};
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
