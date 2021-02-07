4d9e3cdda0f6c710e02d07aafe814b3e753d03d6
kelseck <kelseyeckelberry@gmail.com>
2021-02-06 20:08:36 -0600

    fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,24 +38,12 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-//Need to alter this further
-
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(DATA_CACHE_NAME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
 });
